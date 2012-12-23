6d1adece37977f145bb85da56844d9fd43b8e90c
basile bouquet <bas.bouquet@gmail.com>
2012-12-23 11:23:48 -0800

    first commit'

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RailsinstallerDemo::Application.config.secret_token = '35de320ca4d6d09cd0d8a3ec51054ce992a2e96f46e1e8149ef294c259ed505c54c87092ce215e707b10271bf444ee127d4cdcaeace1fc144bfcf93d93277832'
